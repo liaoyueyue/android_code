eea98d8a31f6f99f67fd50c963c5e3efa80f4efe
liaoyueyue <973044869@qq.com>
2024-05-26 02:34:20 +0800

    update: 20240526

---
+++ Experiment1/app/src/main/java/com/example/experiment1/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.experiment1;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.calculator_layout);
+    }
+}
